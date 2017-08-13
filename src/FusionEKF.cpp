d3dab6cd921cd89f719a3f942104931936df27dc
Jayasim <jayasim@gmail.com>
2017-08-13 11:50:53 +0530

    Convert polar to Caterisan for Radar and Initialize state accordingly for both

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -81,7 +81,25 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
 
-    
+    if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      /**
+       Initialize state.
+      */
+      ekf_.x_(0) = measurement_pack.raw_measurements_(0);
+      ekf_.x_(1) = measurement_pack.raw_measurements_(1);
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      /**
+       Convert radar from polar to cartesian coordinates and initialize state.
+      */
+      float ro     = measurement_pack.raw_measurements_(0);
+      float phi    = measurement_pack.raw_measurements_(1);
+      float ro_dot = measurement_pack.raw_measurements_(2);
+      ekf_.x_(0) = ro     * cos(phi);
+      ekf_.x_(1) = ro     * sin(phi);      
+      ekf_.x_(2) = ro_dot * cos(phi);
+      ekf_.x_(3) = ro_dot * sin(phi);
+    }
 
     previous_timestamp_ = measurement_pack.timestamp_;
 
