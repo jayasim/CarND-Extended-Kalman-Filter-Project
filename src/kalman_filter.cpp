b2df8e5180ce743c2d181c0d27f665dcfe5beef6
Jayasim <jayasim@gmail.com>
2017-08-13 17:25:30 +0530

    Reused the P_*Ht math and angle normalization angle value

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,6 +1,7 @@
 #include <math.h>       /* atan2 */
 #include "kalman_filter.h"
 
+
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -64,10 +65,21 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   VectorXd z_pred(3);
   z_pred << rho, phi, rho_dot;
   VectorXd y = z - z_pred;
+
+  while (y(1)>M_PI){
+      y(1) -= 2 * M_PI;
+  }
+  while (y(1)<-M_PI){
+      y(1) += 2 * M_PI;
+  }
+
+
+
   MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
   MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd Si = S.inverse();
+  //MatrixXd PHt = P_ * Ht;
   MatrixXd K = PHt * Si;
 
   //new estimate
